675641aaed13271d3a23e1f3e54a665457e35c3e
Brandon Kite <brandonkite92@gmail.com>
2022-03-31 19:14:56 -0700

    make docs feature aware on docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Fuel cryptographic primitives.
 
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![warn(missing_docs)]
 // Wrong clippy convention; check
 // https://rust-lang.github.io/api-guidelines/naming.html
